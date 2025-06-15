3f7cd3dc662b162e701f74d7b8dcd8cf5fb1af42
Dongjin-1203 <hambur1203@gmail.com>
2025-06-15 23:13:04 +0900

    dispatcher.py 주석 추가

--- llm_engineering/application/crawlers/dispatcher.py
+++ llm_engineering/application/crawlers/dispatcher.py
@@ -23,21 +23,25 @@ class CrawlerDispatcher:
     def register_medium(self) -> "CrawlerDispatcher":
         self.medium = MediumCrawler()
         return self
+    
     # LinkedIn 크롤러 등록
     def register_linkedin(self) -> "CrawlerDispatcher":
         self.linkedin = LinkedInCrawler()
         return self
+    
     # GitHub 크롤러 등록
     def register_github(self) -> "CrawlerDispatcher":
         self.github = GithubCrawler()
         return self
     
+    # 각 도메인 주소의 형식을 일관된 형식으로 변환
     def register(self, domain: str, crawler: type[BaseCrawler]) -> "None":
         parser_domain = urlparse(domain)
         domain = parser_domain.netloc
 
         self._crawlers[r"https://(wwww\.)?{}?*".format(re.escape(domain))] = crawler
 
+    # 주어진 URL과 등록된 도메인과 비교해 적절한 크롤러인지 결정
     def get_crawler(self, url: str) -> BaseCrawler:
         for pattern, crawler in self._crawlers.items():
             if re.match(pattern, url):
